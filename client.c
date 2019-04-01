fc1e2298c98aa94056de7559fd3c7fe22a29247b
HarshGundecha <harsh.gundecha@gmail.com>
2019-04-01 22:01:37 +0530

    code refactored

--- client.c
+++ client.c
@@ -3,71 +3,75 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <netdb.h>
+
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
 //include <netinet/in.h>
 //include <arpa/inet.h>
 
-#define	MAXLINE	 8192  /* Max text line length */
-
+#define	MAXLINE	 8192 
 int open_clientfd(char *hostname, char *port)
 {
-	int clientfd, flags;
+	int clientfd, flags=NI_NUMERICHOST | NI_NUMERICSERV;
 	struct addrinfo hints, *listp, *p;
 	char host[MAXLINE],service[MAXLINE];
-
-	/* Get a list of potential server addresses */
 	memset(&hints, 0, sizeof(struct addrinfo));
-	hints.ai_socktype = SOCK_STREAM;  /* Open a connection */
-	hints.ai_flags = AI_NUMERICSERV;  /* ... using a numeric port arg. */
-	hints.ai_flags |= AI_ADDRCONFIG;  /* Recommended for connections where we get IPv4 or IPv6 addresses */
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_NUMERICSERV | AI_ADDRCONFIG;
 	getaddrinfo(hostname, port, &hints, &listp);
-
-	/* Walk the list for one that we can successfully connect to */
 	for (p = listp; p; p = p->ai_next)
-	{
-		/* Create a socket descriptor */
 		if ((clientfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) < 0) 
-				continue; /* Socket failed, try the next */
-
-		flags = NI_NUMERICHOST | NI_NUMERICSERV; /* Display address string instead of domain name and port number instead of service name */		
-		getnameinfo(p->ai_addr, p->ai_addrlen, host, MAXLINE, service, MAXLINE, flags);
-		printf("host:%s, service:%s\n", host, service);
-	
-		/* Connect to the server */
-		if (connect(clientfd, p->ai_addr, p->ai_addrlen) != -1) 
-		{
-			printf("Connected to server %s at port %s\n", host, service);
-			break; /* Success */
+				continue;
+		else
+		{			
+			getnameinfo(p->ai_addr, p->ai_addrlen, host, MAXLINE, service, MAXLINE, flags);
+			printf("Host : %s | Service : %s\n",host, service);
+			if (connect(clientfd, p->ai_addr, p->ai_addrlen) != -1) 
+			{
+				printf("Connected to Server : %s at Port : %s\n", host, service);
+				break;
+			}
+			close(clientfd);
 		}
-		close(clientfd); /* Connect failed, try another */  //line:netp:openclientfd:closefd
-	} 
-
-	/* Clean up */
 	freeaddrinfo(listp);
-	if (!p) /* All connects failed */
-			return -1;
-	else    /* The last connect succeeded */
-			return clientfd;
+	return (!p?-1:clientfd);
 }
 
+int ReadWriteMessageToFD(int connfd)
+{
+	int tmp;
+	char buf[MAXLINE];
+	printf("\nWrite : ");
+	fflush(stdin);
+	while (fgets(buf, MAXLINE, stdin) != NULL)
+	{
+		write(connfd, buf, strlen(buf));
+		if(strcmp("EXIT\n", buf)==0)
+		{
+			printf("Chat Ended\n");			
+			break;
+		}
+		tmp=read(connfd, buf, MAXLINE);
+		buf[tmp]='\0';
+		printf("Read : " ANSI_COLOR_GREEN"%s" ANSI_COLOR_RESET, buf);
+		if (buf[0] == '\n' || strcmp("EXIT\n", buf)==0)
+		{
+			printf("Chat Ended\n");
+			break;
+		}
+		printf("Write : ");
+	}
+	return connfd;
+}
 
 int main(int argc, char **argv)
 {
-	int clientfd;
-	char *host, *port, buf[MAXLINE];
-	host = argv[1];
-	port = argv[2];
-	clientfd = open_clientfd(host, port);
-	printf("\nEnter message : ");
-	if(clientfd!=-1)
-		while (fgets(buf, MAXLINE, stdin) != NULL)
-		{
-			write(clientfd, buf, strlen(buf));
-			read(clientfd, buf, MAXLINE);
-			fputs(buf, stdout);
-			if (buf[0] == '\n')
-				break;
-			printf("\nEnter message : ");
-		}
-	close(clientfd);
-	exit(0);
+	close( ReadWriteMessageToFD( open_clientfd( argv[1], argv[2]) ) );
+	return 0;
 }
--- server.c
+++ server.c
@@ -5,84 +5,91 @@
 #include<string.h>
 #define MAXLINE 8000
 #define LISTENQ 1000
-typedef struct addrinfo addrinfo2;
-typedef struct sockaddr sockaddr2;
-typedef struct sockaddr_storage sockaddr_storage2;
+
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+typedef struct addrinfo addrinfo;
+typedef struct sockaddr sockaddr;
+typedef struct sockaddr_storage sockaddr_storage;
 //typedef char[MAXLINE] maxchar;
 int open_client_fd(char *port)
 {
-	addrinfo2 hints, *listp, *p;
-	int listenfd, optval=1, flags = NI_NUMERICHOST | NI_NUMERICSERV; 
+	addrinfo hints, *listp, *p;
+	int listenfd, optval=1, flags = NI_NUMERICHOST | NI_NUMERICSERV, errorno=0; 
 	char host[MAXLINE],service[MAXLINE];
 	memset(&hints, 0, sizeof(hints));
 	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags = AI_PASSIVE | AI_ADDRCONFIG;
-	hints.ai_flags |= AI_NUMERICSERV;
+	hints.ai_flags = AI_PASSIVE | AI_ADDRCONFIG | AI_NUMERICSERV;
 	getaddrinfo(NULL, port, &hints, &listp);
 	for(p = listp; p; p = p->ai_next)
 		if((listenfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol))<0)
-		{	
-			printf("Listen Failed\n");
-			continue;			
+		{
+			continue;
+			errorno = -1;
 		}
 		else
 		{
 			setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval, sizeof(int));
 			getnameinfo(p->ai_addr, p->ai_addrlen, host, MAXLINE, service, MAXLINE, flags);
-			printf("host : %s, service : %s\n",host, service);
-
+			printf("Host : %s | Service : %s\n",host, service);
 			if(bind(listenfd, p->ai_addr, p->ai_addrlen) == 0)
-			{
-				printf("Hello, stuck here");
 				break;
-			}
 			else
 			{
-				printf("binding failed");
 				close(listenfd);
+				errorno = -2;
 			}
 		}
 	freeaddrinfo(listp);
-	return ((((listen(listenfd, LISTENQ) < 0) && (close(listenfd) == 0)) || !p) ? -1 : listenfd );
-	// return (!p?-1:listenfd);
+	return ((((listen(listenfd, LISTENQ) < 0) && (close(listenfd) == 0 && (errorno=-3))) || !p) ? errorno : listenfd );
 }
-void echo(int connfd)
+int ReadWriteToFD(int connfd)
 {
 	size_t n;
 	char buf[MAXLINE];
+	int tmp;
 	while((n=read(connfd, buf, MAXLINE)) != 0)
 	{
 		buf[n]='\0';
-		printf("Server received %d bytes\nServer received message : %s\n", (int)n, buf);
-		write(connfd, buf, n);
+		printf("Read : " ANSI_COLOR_GREEN "%s" ANSI_COLOR_RESET, buf);
+		if(strcmp("EXIT\n", buf)==0)
+		{
+			printf("Chat Ended\n");
+			break;
+		}
+		printf("Write : ");
+		fflush(stdin);
+		fgets(buf, MAXLINE, stdin);
+		write(connfd, buf, strlen(buf));
+		if(strcmp("EXIT\n", buf)==0)
+		{
+			printf("Chat Ended\n");			
+			break;
+		}
 	}
+	return connfd;
 }
+
+int GetClientFD(int listenfd)
+{
+	socklen_t clientlen;
+	sockaddr_storage clientaddr;
+	char client_hostname[MAXLINE], client_port[MAXLINE];
+	printf("Waiting for a client to connect\n");
+	clientlen = sizeof(clientaddr);
+	int connfd = accept(listenfd, (sockaddr *)&clientaddr, &clientlen);
+	getnameinfo((sockaddr *)&clientaddr, clientlen, client_hostname, MAXLINE, client_port, MAXLINE, 0);
+	return connfd;
+}
+
 int main(int argc, char* argv[])
 {
-	int listenfd, connfd;
-	socklen_t clientlen;
-	sockaddr_storage2 clientaddr;
-	char client_hostname[MAXLINE], client_port[MAXLINE];
-	listenfd = open_client_fd(argv[1]);
-	printf("out");
-	if(listenfd!=-1)
-		while(1)
-		{
-			printf("wfc2c");
-			//printf("Waiting for a new client to connect\n");
-			clientlen = sizeof(sockaddr_storage2);
-			connfd = accept(listenfd, (sockaddr2 *)&clientaddr, &clientlen);
-			getnameinfo((sockaddr2 *)&clientaddr, clientlen, client_hostname, MAXLINE, client_port, MAXLINE, 0);
-			printf(".");
-			printf("Starting Communication\n");
-			echo(connfd);
-			printf("Ending Communication\n");
-			close(connfd);
-		}
-	else
-	{
-		printf("Listenfd error");
-	}
-	
+	close( ReadWriteToFD( GetClientFD( open_client_fd( argv[1] ) ) ) );
 	return 0;
 }
