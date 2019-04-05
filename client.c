47f87e281ad5fdb4728edf0116f13ac8271ebb48
HarshGundecha <harsh.gundecha@gmail.com>
2019-04-05 22:06:10 +0530

    refactored and cleanup

--- client.c
+++ client.c
@@ -3,29 +3,9 @@
 #include "socketchat.h"
 #include "socketconn.h"
 
-#define OpenSocketFD(hostname, port) OpenSocketFDGeneric('c',hostname,port)
-//#main
+//#main()
 #define ABS(N) ((N<0)?(-N):(N))
-
-
-
-// int SocketChat(int connfd)
-// {
-// 	if(connfd < 0)
-// 		return connfd;
-// 	int ReadCount;
-// 	char TmpStr[MAXLINE];
-// 	while( printf("Write : ") && fgets(TmpStr, MAXLINE, stdin) != NULL)
-// 	{
-// 		RemoveTrailingNewLineChar(TmpStr);
-// 		write(connfd, TmpStr, strlen(TmpStr));
-// 		ExitCheckMacro(TmpStr); //checks for exit message and does so if it is
-// 		TmpStr[read(connfd, TmpStr, MAXLINE)]='\0';
-// 		printf2ndGreen("Read : ", TmpStr);
-// 		ExitCheckMacro(TmpStr);
-// 	}
-// 	return connfd;
-// }
+#define OpenSocketFD(hostname, port) OpenSocketFDGeneric('c',hostname,port)
 
 int main(int argc, char **argv)
 {
--- server.c
+++ server.c
@@ -2,34 +2,12 @@
 #include<unistd.h>
 #include "socketchat.h"
 #include "socketconn.h"
-#define OpenSocketFD(port) OpenSocketFDGeneric('s',NULL,port)
+
 //#main()
 #define ABS(N) ((N<0)?(-N):(N))
+#define OpenSocketFD(port) OpenSocketFDGeneric('s',NULL,port)
 
-//will be splited to 2 seperate () for readinf and writing on parallel execution by threads
-// int SocketChat(int connfd)
-// {
-// 	if(connfd < 0 )
-// 		return connfd;
-// 	size_t ReadCount;
-// 	char TmpStr[MAXLINE];
-// 	while((ReadCount=read(connfd, TmpStr, MAXLINE)) != 0)
-// 	{
-// 		TmpStr[ReadCount]='\0';
-// 		printf("Read : %s\n", TmpStr);
-// 		//printf2ndGreen("Read : ", TmpStr);
-// 		ExitCheckMacro(TmpStr);
-// 		fflush(stdin);
-// 		printf("Write : ");
-// 		fgets(TmpStr, MAXLINE, stdin);
-// 		RemoveTrailingNewLineChar(TmpStr);
-// 		write(connfd, TmpStr, strlen(TmpStr));
-// 		ExitCheckMacro(TmpStr);
-// 	}
-// 	return connfd;
-// }
-
-int main(int argc, char* argv[])
+int main(int argc, char** argv)
 {
 	short tmp = close( SocketChat( GetClientFD( OpenSocketFD( argv[1] ) ) ) );
 	return ABS(tmp);
